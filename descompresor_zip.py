d5f6070335de4ecd774f6db862377a516c5d0b33
Carlos Borja <Carlos@example.com>
2022-12-23 19:30:02 +0100

    Añadiendo descompresor

---
+++ descompresor_zip.py
@@ -0,0 +1,9 @@
+import zipfile
+
+archivo_zip = zipfile.ZipFile('D:\\ficheros_comprimidos\\documentos.zip')
+archivo_zip.extractall('D:\\ficheros_descomprimidos\\')
+
+print("Descomprimiendo:")
+print(archivo_zip.namelist())
+
+archivo_zip.close()
