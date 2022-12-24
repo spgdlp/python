d8801028ffcb38e27a92f17351f66c55993c22f5
spgdlp <71128858+spgdlp@users.noreply.github.com>
2022-12-24 11:35:48 +0100

    Update descompresor_zip.py

--- descompresor_zip.py
+++ descompresor_zip.py
@@ -1,8 +1,8 @@
 import zipfile
 
 #Descomprimir archivos zip
-archivo_zip = zipfile.ZipFile('D:\\ficheros_comprimidos\\documentos.zip')
-archivo_zip.extractall('D:\\ficheros_descomprimidos\\')
+archivo_zip = zipfile.ZipFile('C:\\users\\pc\\Documents\documentos.zip')
+archivo_zip.extractall('C:\\users\\pc\\Documents\\ficheros_descomprimidos\\documentos_extraidos')
 
 print("Descomprimiendo:")
 print(archivo_zip.namelist())
